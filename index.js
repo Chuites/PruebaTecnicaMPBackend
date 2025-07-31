3c3ae29aee443bfaced19783764d1a1a6cae57e9
Carlos Huite <54592726+Chuites@users.noreply.github.com>
2025-07-30 19:00:57 -0600

    Actualizar configuración de la base de datos y agregar conexión a SQL Server

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const fiscaliaRoutes = require("./routes/fiscaliaRoutes");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Ejemplo de ruta temporal
 app.get("/", (req, res) => {
   res.send("API del Ministerio Público en funcionamiento");
 });
 
-const fiscaliaRoutes = require("./routes/fiscaliaRoutes");
 app.use("/api/fiscalias", fiscaliaRoutes);
 
 const PORT = process.env.PORT || 3000;
